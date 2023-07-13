2f15300bbec2249506a441561c923904b023c693
jeffdoeswork <jeff@does.work>
2023-07-13 15:57:17 -0400

    walking works!

--- arduino_nano/arduino_walk.ino
+++ arduino_nano/arduino_walk.ino
@@ -7,25 +7,28 @@ Servo left_knee;
 Servo right_knee;  
 Servo left_thigh;  
 Servo right_thigh;  
+Servo bottom_foot;
+
 
 //SoftwareSerial mySerial(2, 3); // RX, TX
 
-int ankle_pos = 135;   
+int ankle_pos = 80;   
 int center_leg_pos = 180;  
 int left_knee_pos = 80;   
 int right_knee_pos = 70;    
 int left_thigh_pos = 110;    
 int right_thigh_pos = 45;      
+int bottom_foot_pos = 110; 
 
-const int STEPS_COUNT = 6;
-int steps[STEPS_COUNT][6] = {
-  {0, 0, 0, 0, 0, 20}, 
-  {0, -80, 80, 0, 0, 0},   
-  {0, 0, 0, -50, 50, 0},  
-  {0, 0, 0, 50, -50, 0},  
-  {0, 80, -80, 0, 0, 0},
-  {0, 0, 0, 0, 0, -20}, 
-  
+const int STEPS_COUNT = 7;
+int steps[STEPS_COUNT][7] = {
+  {0, -50, 50, 0, 0, 0, 0},
+  {-180, 0, 0, -20, 20, 0, 0},
+  {0, 0, 0, 0, 0, -70, 0},
+  {0, 50, -50, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 30, 0},
+  {0, 0, 0, 20, -20, 40, 0},
+  {180, 0, 0, 0, 0, 0, 0},
 };
 
 void setup() {
@@ -38,6 +41,7 @@ void setup() {
   right_knee.attach(11);  
   left_thigh.attach(9); 
   right_thigh.attach(8); 
+  bottom_foot.attach(10); 
 
     // initialize servos to the upright position
   ankle.write(ankle_pos);
@@ -46,11 +50,12 @@ void setup() {
   right_knee.write(right_knee_pos);
   left_thigh.write(left_thigh_pos);
   right_thigh.write(right_thigh_pos);
+  bottom_foot.write(bottom_foot_pos);
 
-  delay(5000); // wait for 5 seconds
+
+  delay(7000); // wait for 5 seconds
 
 }
-
 void updateServoPos(Servo &servo, int &current_pos, int target_pos) {
   if (current_pos < target_pos) {
     current_pos++;
@@ -59,8 +64,6 @@ void updateServoPos(Servo &servo, int &current_pos, int target_pos) {
   }
   servo.write(current_pos);
 }
-
-
 void walkSequence() {
   for(int step = 0; step < STEPS_COUNT; ++step) {
     // Update target positions by adding deltas
@@ -70,7 +73,8 @@ void walkSequence() {
     int left_knee_target = left_knee_pos + steps[step][3];
     int right_knee_target = right_knee_pos + steps[step][4];
     int ankle_target = ankle_pos + steps[step][5];
-    
+    int bottom_foot_target = bottom_foot_pos + steps[step][6];
+
     // Make sure positions are in valid range
     center_leg_target = constrain(center_leg_target, 0, 180);
     left_thigh_target = constrain(left_thigh_target, 0, 180);
@@ -78,11 +82,15 @@ void walkSequence() {
     left_knee_target = constrain(left_knee_target, 0, 180);
     right_knee_target = constrain(right_knee_target, 0, 180);
     ankle_target = constrain(ankle_target, 0, 180);
+    bottom_foot_target = constrain(bottom_foot_target, 0, 180);
+
 
     // Wait until all servos have reached their target positions
     while(center_leg.read() != center_leg_target || left_thigh.read() != left_thigh_target || 
-          right_thigh.read() != right_thigh_target || left_knee.read() != left_knee_target || 
-          right_knee.read() != right_knee_target || ankle.read() != ankle_target) {
+      right_thigh.read() != right_thigh_target || left_knee.read() != left_knee_target || 
+      right_knee.read() != right_knee_target || ankle.read() != ankle_target || 
+      bottom_foot.read() != bottom_foot_target) {
+
       // Update servo positions
       updateServoPos(center_leg, center_leg_pos, center_leg_target);
       updateServoPos(left_thigh, left_thigh_pos, left_thigh_target);
@@ -90,15 +98,14 @@ void walkSequence() {
       updateServoPos(left_knee, left_knee_pos, left_knee_target);
       updateServoPos(right_knee, right_knee_pos, right_knee_target);
       updateServoPos(ankle, ankle_pos, ankle_target);
+      updateServoPos(bottom_foot, bottom_foot_pos, bottom_foot_target);
+
       
       delay(10); // Slow down the servo movements
     }
   }
 }
-
-
 void loop() {
-    walkSequence();
-    delay(1000); 
+    //walkSequence();
+    delay(2000); 
 }
-
